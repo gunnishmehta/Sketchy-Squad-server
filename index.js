438da5be0944dae69d06ef387046b5fddb4ceab2
darkmoon-6524 <gunnishmehta2003@gmail.com>
2024-02-25 22:07:37 +0530

    implemented cors

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ dotenv.config()
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const server = http.createServer(app);
 
