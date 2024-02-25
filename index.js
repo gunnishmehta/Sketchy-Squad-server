c24eb84e754d78650e698fa3e486f703e3e370f7
darkmoon-6524 <gunnishmehta2003@gmail.com>
2024-02-25 12:54:49 +0530

    added env variables

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ let changeRequest = true;
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
